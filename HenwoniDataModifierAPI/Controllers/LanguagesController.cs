ef57a3ec8b3c7041375c9efecd83f6b046ff3d0f
Paul <repostopsy@gmail.com>
2025-05-20 06:55:54 +0100

    Progress

---
+++ HenwoniDataModifierAPI/Controllers/LanguagesController.cs
@@ -0,0 +1,38 @@
+﻿using HenwoniDataModifierAPI.Data;
+using HenwoniDataModifierAPI.Models.Common;
+using HenwoniDataModifierAPI.Models.Location;
+using HenwoniDataModifierAPI.Utilities;
+using HenwoniDataModifierAPI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HenwoniDataModifierAPI.Controllers
+{
+    [Route("api/languages")]
+    [ApiController]
+    public class LanguagesController : ControllerBase
+    {
+        public readonly ApplicationDbContext _context;
+        public LanguagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Language>> UpdateTemplateDescriptionAsync(LanguagePostRequest request)
+        {
+            Language existing = await _context.Languages.Where(x => x.SystemName == request.SystemName).FirstOrDefaultAsync();
+            if (existing == null)
+            {
+                // Create it.
+                existing = new Language();
+                _context.Languages.Add(existing);
+            }
+            existing.CopyPropertiesFrom(request);
+            await _context.SaveChangesAsync();
+            return existing;
+        }
+    }
+}
---
+++ HenwoniDataModifierAPI/ViewModels/LanguagePostRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace HenwoniDataModifierAPI.ViewModels
+{
+    public class LanguagePostRequest
+    {
+        public string Title { get; set; }
+        public string? Flag { get; set; }
+        public string SystemName { get; set; }
+        public string? LocaleTitle { get; set; }
+    }
+}
